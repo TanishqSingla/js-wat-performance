0aefc8d296362c864ce96a19cf4021db4667c71c
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-25 19:14:28 +0530

    Adding shuffled array

--- public/index.js
+++ public/index.js
@@ -1,1 +1,18 @@
-function start() {}
+function shuffle(array) {
+  let currentIndex = array.length - 1;
+  do {
+    let randomIndex = Math.floor(Math.random() * array.length);
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  } while (currentIndex--);
+
+  return array;
+}
+
+function start() {
+  const array = shuffle([...Array(100).keys()]);
+  console.log(array);
+}
