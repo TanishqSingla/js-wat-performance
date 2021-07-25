a10d048edcc4487ddd8a7fc7b70e40a1236be2c6
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-26 00:50:03 +0530

    Added js bubble sort
    removed tests

--- public/index.js
+++ public/index.js
@@ -12,7 +12,16 @@ function shuffle(array) {
   return array;
 }
 
+function JSBubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+}
+
 function start() {
   const array = shuffle([...Array(100).keys()]);
-  console.log(array);
 }
