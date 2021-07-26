ea1d7ae84e2978f5e16d70c1033c14b516c3f988
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-27 02:21:50 +0530

    Added loader and instantiated wasm

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,3 +1,16 @@
 // The entry file of your WebAssembly module.
+export function ASbubbleSort(arr: Int32Array): Int32Array {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
 
-export function bubbleSort() {}
+  return arr;
+}
+
+export const Int32Array_ID = idof<Int32Array>();
--- public/index.js
+++ public/index.js
@@ -1,5 +1,9 @@
 const timeTaken = document.getElementById("time-taken");
 
+// memory for assemblyscript
+var memory = null;
+const wasm = fetch("optimized.wasm");
+
 function shuffle(array) {
   let currentIndex = array.length - 1;
   do {
@@ -15,6 +19,8 @@ function shuffle(array) {
 }
 
 function JSBubbleSort(arr) {
+  let startTime = new Date().getTime();
+
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -22,13 +28,33 @@ function JSBubbleSort(arr) {
       }
     }
   }
+
+  timeTaken.innerHTML = `${new Date().getTime() - startTime}`;
+}
+
+function ASBubbleSort(array) {
+  (async () => {
+    const { instance } = await loader.instantiate(wasm);
+
+    console.log(instance);
+
+    const { ASbubbleSort, Int32Array_ID } = instance.exports;
+    const { __newArray } = instance.exports;
+    const { __getArray } = instance.exports;
+
+    const importArrPtr = __newArray(Int32Array_ID, array);
+    const exportedArrayPtr = ASbubbleSort(importArrPtr);
+    const values = __getArray(exportedArrayPtr);
+    console.log(values);
+  })();
 }
 
 function start() {
-  const array = shuffle([...Array(4000).keys()]);
+  const shuffledArray = shuffle([...Array(4000).keys()]);
 
-  let startTime = new Date().getTime();
-  JSBubbleSort(array);
-  console.log(new Date().getTime() - startTime);
-  timeTaken.innerHTML = `${new Date().getTime() - startTime}`;
+  JSBubbleSort(shuffledArray);
+
+  ASBubbleSort(shuffledArray);
 }
+
+document.querySelector("button").addEventListener("click", start);
